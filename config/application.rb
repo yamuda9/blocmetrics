80e39f2e801a1b13b82048dea7025d30e4b09189
Alvin Andal <alvin.009.andal@gmail.com>
2015-11-24 19:40:47 -0800

    Added fix to push to Heroku

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Blocmetrics
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # assets:precompile task failure during push to Heroku fix:
+    config.assets.initialize_on_precompile = false
   end
 end
