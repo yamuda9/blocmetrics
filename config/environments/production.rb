f87414dab420137f1b31e13bb327ad1560f848c8
Alvin Andal <alvin.009.andal@gmail.com>
2016-02-04 12:43:10 -0800

    Added flash messages and devise views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://salty-island-5717.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'https://salty-island-5717.herokuapp.com'
+  }
 end
